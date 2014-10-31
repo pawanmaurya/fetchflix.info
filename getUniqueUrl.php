102ce1de2b1b6024bdc4031d02158ea3e25260ff
unknown <pawanm@pc-pawanm1.global.cadence.com>
2014-10-31 22:21:46 +0530

    sds

--- getUniqueUrl.php
+++ getUniqueUrl.php
@@ -6,7 +6,8 @@ ini_set("display_errors", 1);
 include_once("db.php");
 
 $db = new DB();
-$movieData = $_POST['movieData'];
+//$movieData = $_POST['movieData'];
+$movieData = array("batman":array('asa',2,'ass'));
 $movieData = serialize($movieData);
 
 $uniqueUrlId = $db->insertData($movieData);
