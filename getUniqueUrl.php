49d40b6ed790d18d45dcc61fa46ec21729b0273e
unknown <pawanm@pc-pawanm1.global.cadence.com>
2014-11-01 19:48:53 +0530

    sdsd

--- getUniqueUrl.php
+++ getUniqueUrl.php
@@ -1,30 +1,18 @@
 <?
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
-//echo parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
+
+if (parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST) != $_SERVER['SERVER_NAME'])
+	echo "sds";
 
 include_once("db.php");
 
 $db = new DB();
 $movieData = $_POST['movieData'];
-//$movieData = array("batman"=>array('asa',2,'ass'));
 $movieData = serialize($movieData);
 
 $uniqueUrlId = $db->insertData($movieData);
 if($uniqueUrlId)
 	echo $uniqueUrlId;
-
-/*
-$uniqueId = uniqid();
-$result = pg_query($pg_conn, "select * from unique_id_to_movies");
-print "<pre>\n";
-if (!pg_num_rows($result)) {
-  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
-} else {
-  print "Tables in your database:\n";
-  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
-}
-print "\n";
- */
 ?>
 
